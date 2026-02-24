33fc2759a0b1cef06166bcd8dd6c5be14bfc1209
Mostakim <mostakim2005arafat@gmail.com>
2026-02-24 19:22:48 +0600

    done

--- machine.js
+++ machine.js
@@ -1,63 +1,81 @@
 console.log("function connected.")
-function idtext(id){
-   return document.getElementById(id).innerText
+function idtext(id) {
+    return document.getElementById(id).innerText
 }
 let jobCount = document.getElementsByClassName('jobCount')
 let cardPart = document.querySelectorAll('main div section')
 // button function
-document.getElementById('interviewList').addEventListener('click',function(){
+document.getElementById('interviewList').addEventListener('click', function () {
     //hiding card part
-    for(const i of cardPart){
+    for (const i of cardPart) {
+        i.classList.add('hidden')
+    }
+    // hide null part
+    document.getElementById('nullPart').classList.add('hidden')
+
+    if (idtext('interviewCount') == 0) {
+        document.getElementById('nullPart').classList.remove('hidden')
+    }
+    else {
+        //show interview list
+        let count =0 
+        for(const i of Btn){
+            if(i.innerText === 'interviewed'){
+                i.parentNode.parentNode.classList.remove('hidden')
+                count = count + 1
+            }
+        }
+        document.getElementById('interviewCount').innerText = count
+
+    }
+})
+document.getElementById('rejectList').addEventListener('click', function () {
+    //hiding card part
+    for (const i of cardPart) {
         i.classList.add('hidden')
     }
     document.getElementById('nullPart').classList.add('hidden')
-    if(idtext('interviewCount') == 0){
+    if (idtext('rejectCount') == 0) {
         document.getElementById('nullPart').classList.remove('hidden')
     }
-    else{
-        //unhide them who has batch interviewed.
-       console.log( )
+    else {
+         //unhide them who has batch interviewed.
+         let count = 0 
+        for(const i of Btn){
+            if(i.innerText === 'rejected'){
+                i.parentNode.parentNode.classList.remove('hidden')
+                count = count + 1
+            }
+        }
+         document.getElementById('rejectCount').innerText = count
+
     }
 
 })
-document.getElementById('rejectList').addEventListener('click',function(){
-   //hiding card part
-    for(const i of cardPart){
+document.getElementById('allList').addEventListener('click', function () {
+    //hiding card part
+    for (const i of cardPart) {
         i.classList.add('hidden')
     }
     document.getElementById('nullPart').classList.add('hidden')
-    if(idtext('rejectCount') == 0){
-        document.getElementById('nullPart').classList.remove('hidden')
-    }
-    else{
-
-    }
-
-})
-document.getElementById('allList').addEventListener('click',function(){
-   //hiding card part
-    for(const i of cardPart){
-        i.classList.add('hidden')
-    }
-    document.getElementById('nullPart').classList.add('hidden')
-    if( jobCount.innerText !== 0){
-        for(const i of cardPart){
+    if (jobCount.innerText !== 0) {
+        for (const i of cardPart) {
             i.classList.remove('hidden')
         }
     }
-    else{
-
+    else {
+       document.getElementById('nullPart').classList.remove('hidden')
     }
 
 })
 // delete icon
 const deleteIcon = document.querySelectorAll('main section i')
-for(const i of deleteIcon){
-    i.addEventListener('click',function(){
+for (const i of deleteIcon) {
+    i.addEventListener('click', function () {
         i.parentNode.parentNode.parentNode.classList.add('hidden')
         // change of jobCount
-        for(const i of jobCount){
-            i.innerText = Number(i.innerText)-1
+        for (const i of jobCount) {
+            i.innerText = Number(i.innerText) - 1
         }
     })
 }
@@ -65,32 +83,33 @@ for(const i of deleteIcon){
 
 // job count
 const Btn = document.querySelectorAll('main section button')
-for(const i of Btn){
-    if(i.innerText === "INTERVIEW"){
+
+for (const i of Btn) {
+    if (i.innerText === "INTERVIEW") {
         const ielement = i.parentNode.parentNode.querySelector('button')
-        i.addEventListener('click',function(){
-             //increase interview count
-               if(ielement.innerText === 'rejected' || ielement.innerText === "NOT APPLIED"){
-           document.getElementById('interviewCount').innerText = Number(idtext('interviewCount')) + 1
-               }
+        i.addEventListener('click', function () {
+            //increase interview count
+            if (ielement.innerText === 'rejected' || ielement.innerText === "NOT APPLIED") {
+                document.getElementById('interviewCount').innerText = Number(idtext('interviewCount')) + 1
+            }
             //add badge 
             ielement.innerText = "interviewed"
             ielement.style.backgroundColor = '#BBF7D0'
             ielement.style.color = '#16A34A'
-           
-            //show in interview page
+
+            
 
         })
     }
-    else if (i.innerText === "REJECTED"){
-                const ielement = i.parentNode.parentNode.querySelector('button')
-        i.addEventListener('click',function(){
-               //increase interview count
-               if(ielement.innerText === 'interviewed' || ielement.innerText === "NOT APPLIED"){
-           document.getElementById('rejectCount').innerText = Number(idtext('rejectCount')) + 1
-               }
-         //add badge
-         i.parentNode.parentNode.querySelector('button').innerText = "rejected"
+    else if (i.innerText === "REJECTED") {
+        const ielement = i.parentNode.parentNode.querySelector('button')
+        i.addEventListener('click', function () {
+            //increase interview count
+            if (ielement.innerText === 'interviewed' || ielement.innerText === "NOT APPLIED") {
+                document.getElementById('rejectCount').innerText = Number(idtext('rejectCount')) + 1
+            }
+            //add badge
+            i.parentNode.parentNode.querySelector('button').innerText = "rejected"
             i.parentNode.parentNode.querySelector('button').style.backgroundColor = '#FECACA'
             i.parentNode.parentNode.querySelector('button').style.color = '#EF4444'
 
@@ -99,5 +118,14 @@ for(const i of Btn){
         })
     }
 }
-    
+
+// color toggle
+function design(id){
+   
+    document.getElementById(id).style.backgroundColor = '#570DF8'
+    document.getElementById(id).style.color = 'white'
+}
+
+
 // job toggle
+
